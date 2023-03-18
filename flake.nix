7b1d2d44042d83726379f7f456f67504dacf455b
Maurice Zhou <jasper@apvc.uk>
2023-03-18 09:28:30 +0100

    add unstable channel in comments
    Signed-off-by: Maurice Zhou <jasper@apvc.uk>

--- flake.nix
+++ flake.nix
@@ -1,11 +1,15 @@
 {
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
+    # stable
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
+
+    # unstable
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # home-manager.url = "github:nix-community/home-manager";
+
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager }@inputs:
