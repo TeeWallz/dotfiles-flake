0437dfe282e3175ca8afcfb0d96239a8aa86c156
Maurice Zhou <jasper@apvc.uk>
2023-03-19 22:16:22 +0100

    fix ssh private key permission
    Signed-off-by: Maurice Zhou <jasper@apvc.uk>

--- modules/per-user/yc/home-manager/not-nix-config-files/bashrc-config.sh
+++ modules/per-user/yc/home-manager/not-nix-config-files/bashrc-config.sh
@@ -237,6 +237,7 @@ done
 EOF
     if test -f $HOME/.ssh/yc && ! test -f /oldroot/$HOME/.ssh/yc; then
 	mv $HOME/.ssh/yc /oldroot/$HOME/.ssh/yc
+	chmod u=rw,go= /oldroot/$HOME/.ssh/yc
     elif ! test -f $HOME/.ssh/yc && ! test -f /oldroot/$HOME/.ssh/yc; then
 	echo "ERROR: private ssh key yc not found!!!"
 	return 1
