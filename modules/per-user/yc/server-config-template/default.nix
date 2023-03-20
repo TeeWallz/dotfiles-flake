407f1ac07ac4d7c88c76968bb87075311a0a4fa7
Maurice Zhou <jasper@apvc.uk>
2023-03-20 07:01:58 +0100

    fix semicolon position
    Signed-off-by: Maurice Zhou <jasper@apvc.uk>

--- modules/per-user/yc/server-config-template/default.nix
+++ modules/per-user/yc/server-config-template/default.nix
@@ -170,9 +170,9 @@ in {
     };
     environment.shellAliases = {
       Nb =
-        "if test -n $TMUX; then echo 'not in tmux'; else git -C /etc/nixos pull && nixos-rebuild boot --option substituters https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store fi;";
+        "if test -n $TMUX; then echo 'not in tmux'; else git -C /etc/nixos pull && nixos-rebuild boot --option substituters https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store; fi";
       Ns =
-        "if test -n $TMUX; then echo 'not in tmux'; else git -C /etc/nixos pull && nixos-rebuild switch --option substituters https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store fi;";
+        "if test -n $TMUX; then echo 'not in tmux'; else git -C /etc/nixos pull && nixos-rebuild switch --option substituters https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store; fi";
       tm = "tmux attach-session";
       e = "$EDITOR";
     };
