1bf7814329ee426e1a42ef26b629e834bce2a08b
Maurice Zhou <jasper@apvc.uk>
2023-03-20 11:26:07 +0100

    NixOS on ZFS: use system type place holder
    Signed-off-by: Maurice Zhou <jasper@apvc.uk>

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         exampleHost = let
           # if aarch64, change to aarch64-linux
           # check with "uname -m" command
-          system = "x86_64-linux";
+          system = "systemType_placeholder";
           pkgs = nixpkgs.legacyPackages.${system};
         in my.lib.mkHost (import ./hosts/exampleHost { inherit system pkgs; });
         qinghe = let
